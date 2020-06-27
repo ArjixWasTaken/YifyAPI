fdfe3933b86146b392bb16e3e3a0c9019add9531
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-28 00:18:18 +0300

    Add files via upload

---
+++ test/ProxyFinder.py
@@ -0,0 +1,29 @@
+import asyncio, click
+from proxybroker import Broker
+import os
+
+async def save(proxies, filename):
+    """Save proxies to a file."""
+    with open(filename, 'w') as f:
+        while True:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            proto = 'https' if 'HTTPS' in proxy.types else 'http'
+            row = '%s://%s:%d\n' % (proto, proxy.host, proxy.port)
+            f.write(row)
+
+
+def get_proxy():
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(
+        broker.grab(countries=['US', 'GB'], limit=1),
+        save(proxies, filename='proxies.txt'),
+    )
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tasks)
+    with open('proxies.txt', 'r') as f:
+    	proxy = f.read().replace('\n', '')
+    os.remove('proxies.txt')
+    return proxy
--- test/testme.py
+++ test/testme.py
@@ -1,4 +1,6 @@
 from YifyAPI import yify as api
-response = api.search_yify('avengers: endgame')[0]
+from ProxyFinder import get_proxy as gp
+proxy = gp()
+response = api.search_yify('avengers: endgame', proxy=proxy)[0]
 result = [response['title'], response['year'], response['qualities']['720p']['size'], response['qualities']['720p']['magnet']]
 print(result)
