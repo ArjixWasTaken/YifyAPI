5af0e656d1edde82b55102d0d3765519bbb6c341
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-27 23:43:11 +0300

    Update testme.py

--- test/testme.py
+++ test/testme.py
@@ -1,3 +1,4 @@
 from YifyAPI import yify as api
 response = api.search_yify('avengers: endgame')[0]
-print(response['title'], response['year'], response['qualities']['720p']['size'], response['qualities']['720p']['magnet'])
+result = [response['title'], response['year'], response['qualities']['720p']['size'], response['qualities']['720p']['magnet']]
+print(result)
