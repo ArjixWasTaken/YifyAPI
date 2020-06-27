b7b95ed980388ff41868cdb4ce7ba3bd64b2053e
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-27 23:37:22 +0300

    Delete versioning.py

--- versioning.py
+++
@@ -1,8 +0,0 @@
-def get_version():
-	import feedparser
-	r = feedparser.parse('https://pypi.org/rss/project/yifyapi/releases.xml')['entries'][0]['title']
-	f = feedparser.parse('https://pypi.org/rss/project/yifyapi/releases.xml')['entries'][0]['title']
-	l = int(r.split('.')[-1]) + 1
-	__version__ = f.split('.')
-	__version__ = __version__[0] + '.' + __version__[1] + '.' + str(l)
-	return __version__
