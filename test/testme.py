4747735e8f35dd2396089e5b65a079e25f60d8c1
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-28 00:34:12 +0300

    Update testme.py

--- test/testme.py
+++ test/testme.py
@@ -1,6 +1,8 @@
 from YifyAPI import yify as api
 from ProxyFinder import get_proxy as gp
+import time
 proxy = gp()
 response = api.search_yify('avengers: endgame', proxy=proxy)[0]
+time.sleep(3)
 result = [response['title'], response['year'], response['qualities']['720p']['size'], response['qualities']['720p']['magnet']]
 print(result)
